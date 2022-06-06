3bddb9df756de6505621dc7f252243c045adc3b4
Anags1 <anagslidia1@gmail.com>
2022-06-06 19:54:05 -0300

    continuaçao aula namespaces

--- namespacespt1.php
+++ namespacespt1.php
@@ -2,25 +2,70 @@
 
     namespace A; 
 
-    class Funcionario implements CadastroInterface {
+    class Funcionario implements \B\CadastroInterface 
+    {
         public $nome = 'Tiago';
-        public function __get($attr) {
+
+        public function __construct()
+        {
+            echo '<pre>';
+            print_r(get_glass_methods(this));
+            echo '</pre>';
+        }
+
+        public function __get($attr) 
+        {
             return $this->$attr;  
+        }
+
+        public function salvar()
+        {
+            echo 'Salvar';
+        }
+
+        public function remover()
+        {
+            echo 'Remover';
         }
     }
 
-    interface CadastroInterface {
+    interface CadastroInterface 
+    {
         public function salvar();
     }
 
-
     namespace B; 
 
-    class Funcionario implements CadastroInterface {
+    class Funcionario implements \A\CadastroInterface 
+    {
         public $nome = 'Heitor';
-        public function __get($attr) {
+
+        public function __construct()
+        {
+            echo '<pre>';
+            print_r(get_glass_methods(this));
+            echo '</pre>';
+        }
+
+        public function __get($attr) 
+        {
             return $this->$attr;
         }
+
+        public function remover()
+        {
+            echo 'Remover';
+        }
+
+        public function salvar()
+        {
+            echo 'Salvar';
+        }
+    }
+
+    interface CadastroInterface
+    {
+        public function remover();
     }
 
 
